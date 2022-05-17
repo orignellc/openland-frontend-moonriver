2c50e639541bbd587ae1ee13457f578f8ad9bc50
alaribebright <alaribe.bright@origne.com>
2022-05-17 13:57:37 -0700

    Add token in dotnev

--- services/uploadPropertyToIpfs.ts
+++ services/uploadPropertyToIpfs.ts
@@ -3,7 +3,7 @@ import { Web3Storage } from "web3.storage";
 export const uploadPropertyToIpfs = async (property: any) => {
     try {
         // Upload Property to IPFS
-        const client = new Web3Storage({ token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDQ0MmMzZTk4NGNhY0MxODZCMDVCY2IyNGMyZUQzN2VBNDQ1OEJFMGQiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NTI3MTQ0NTE5MTUsIm5hbWUiOiJvcGVubGFuZCJ9.Z2BBkxc7cQ9Ot5ZD8_LLAqSA4ck9fgNUrwJjoIzj9Zg" })
+        const client = new Web3Storage({ token: process.env.W3STORAGE_TOKEN! })
 
         // const files = makeFileObjects()
         const cid = await client.put(property)
